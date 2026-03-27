c602ae853c92078f7f24476d5766a6d0095e8995
pedrorc <pedrorc313@gmail.com>
2026-03-27 15:01:06 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'habitflow-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.svg'];
+const CACHE = 'habitflow-v2';
+const ASSETS = ['/Habitflow/', '/Habitflow/index.html', '/Habitflow/manifest.json', '/Habitflow/icon.svg'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -15,6 +15,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/Habitflow/index.html')))
   );
 });
